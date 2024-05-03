83cc25850d1d2834576e51e93d99ac06e86473c1
hector <hx.euwx@gmail.com>
2024-05-03 17:13:48 -0400

    getting fancy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import 'tailwindcss/tailwind.css';
@@ -7,28 +7,35 @@ import ParticlesComponent from '../components/Particles';
 import photos from '../components/photos';
 
 export default function Home() {
-    // États locaux pour la photo sélectionnée et l'état de la modale
     const [selectedPhoto, setSelectedPhoto] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [pageOpacity, setPageOpacity] = useState(0);
+    const [modalOpacity, setModalOpacity] = useState(0);
 
-    // Fonction pour ouvrir la modale
-    const openModal = (photo: any) => {
+    useEffect(() => {
+        const fadeInTimeout = setTimeout(() => {
+            setPageOpacity(1);
+        }, 100);
+
+        return () => clearTimeout(fadeInTimeout);
+    }, []);
+
+    const openModal = (photo) => {
         setSelectedPhoto(photo);
         setIsModalOpen(true);
+        setModalOpacity(0);
+        setTimeout(() => setModalOpacity(1), 100);
     };
 
-    // Fonction pour fermer la modale
     const closeModal = () => {
         setSelectedPhoto(null);
         setIsModalOpen(false);
     };
 
-    // Composant modale personnalisé
-    const Modal = ({ photo, onClose }: { photo: any; onClose: () => void }) => {
+    const Modal = ({ photo, onClose }) => {
         if (!photo) return null;
 
-        const handleModalClick = (event: React.MouseEvent<HTMLDivElement>) => {
-            // Fermer la modale si l'utilisateur clique en dehors de l'image
+        const handleModalClick = (event) => {
             if (event.target === event.currentTarget) {
                 onClose();
             }
@@ -38,10 +45,11 @@ export default function Home() {
             <div
                 className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
                 onClick={handleModalClick}
+                style={{ opacity: modalOpacity, transition: 'opacity 300ms ease-in-out' }}
             >
                 <div className="relative">
                     <button
-                        className="absolute top-2 right-2 text-white text-2xl"
+                        className="text-white text-2xl absolute top-2 right-2 transition-colors duration-300 ease-in-out hover:text-gray-300"
                         onClick={onClose}
                     >
                         &times;
@@ -60,49 +68,53 @@ export default function Home() {
         );
     };
 
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
+    return (  
+        <div className="min-h-screen" style={{ opacity: pageOpacity, transition: 'opacity 2000ms ease-in-out' }}>
             <Head>
                 <title>Erwan Mouton</title>
                 <meta name="description" content="Photographer based in Paris" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-
-            <header className="flex justify-between w-full px-10 py-8">
-                <a href="#" className="text-white hover:text-gray-300">Projects</a>
-                <a href="#" className="text-white hover:text-gray-300">Contact</a>
+            <div className="flex flex-col justify-center text-center h-screen">
+            <header className="sticky justify-between w-full">
+                <a href="#" className="text-white hover:text-gray-300 mr-5">Projects</a>
+                <a href="#" className="text-white hover:text-gray-300 ">Contact</a>
             </header>
 
-            <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+            {/* Section pour le nom et le prénom */}
+            <section className="flex flex-col items-center justify-center text-center">
                 <h1 className="text-6xl font-bold text-white">Erwan Mouton</h1>
                 <p className="mt-3 text-white">Photographer, based in Paris</p>
-                <ParticlesComponent id="particles"/>
-            </main>
-
-            <div className="container mx-auto p-4">
-                {/* Galerie de photos */}
-                <div style={{ columnCount: 5, columnGap: '1rem' }}>
-                    {photos.map((photo, index) => (
-                        <div
-                            key={photo.id}
-                            style={{ marginBottom: '1rem' }}
-                            onClick={() => openModal(photo)}
-                        >
-                            <Image
-                                src={photo.src}
-                                alt={photo.alt}
-                                width={200}
-                                height={200}
-                                layout="responsive"
-                                objectFit="cover"
-                                className="rounded-md w-full"
-                            />
-                        </div>
-                    ))}
-                </div>
+                <ParticlesComponent id="particles" />
+            </section>
             </div>
+            {/* Section pour la galerie de photos */}
+            <section className="w-full bg-black py-4 border-t border-gray-800" style={{ opacity: pageOpacity, transition: 'opacity 500ms ease-in-out' }}>
+                <div className="container mx-auto ">
+                    <div style={{ columnCount: 5, columnGap: '1rem' }}>
+                        {photos.map((photo) => (
+                            <div
+                                key={photo.id}
+                                style={{ marginBottom: '1rem' }}
+                                onClick={() => openModal(photo)}
+                                className="transition-transform duration-300 ease-in-out hover:scale-105"
+                            >
+                                <Image
+                                    src={photo.src}
+                                    alt={photo.alt}
+                                    width={200}
+                                    height={200}
+                                    layout="responsive"
+                                    objectFit="cover"
+                                    className="rounded-md w-full"
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
 
-            <footer className="flex justify-center w-full py-8 border-t border-gray-800">
+            <footer className="flex justify-center w-full py-8 border-t border-gray-800 bg-black">
                 <p className="text-white">© 2024 hctr. All rights reserved.</p>
             </footer>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      transitionDuration: {
+        '0': '0ms',
+        '300': '300ms',
+        '500': '500ms',
+        '750': '750ms',
+        '1000': '1000ms',
+    },
+    transitionTimingFunction: {
+        'ease-in': 'cubic-bezier(0.4, 0, 1, 1)',
+        'ease-out': 'cubic-bezier(0, 0, 0.2, 1)',
+        'ease-in-out': 'cubic-bezier(0.4, 0, 0.2, 1)',
+        'linear': 'linear',
+    },
     },
   },
   plugins: [],
