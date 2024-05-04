ea7d6de90c86d2e9966e8a3069537c9b0394fd9f
hector <hx.euwx@gmail.com>
2024-05-03 23:02:18 -0400

    encore!

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import 'tailwindcss/tailwind.css';
 import ParticlesComponent from '../components/particles.js';
 import photos from '../components/photos';
 
