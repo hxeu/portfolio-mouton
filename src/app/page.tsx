356e638760ec2d6086cd3409a018b24023dd0d53
hector <hx.euwx@gmail.com>
2024-05-03 22:39:11 -0400

    trying to deploy on vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import 'tailwindcss/tailwind.css';
-import ParticlesComponent from '../components/Particles';
+import ParticlesComponent from '../components/particles.js';
 import photos from '../components/photos';
 
 export default function Home() {
